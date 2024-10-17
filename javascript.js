4ff97cd8c096f88a27c11eea5a5a8ffe37b6694f
Chetan <165123379+mn-chetan@users.noreply.github.com>
2024-10-17 18:40:04 +0530

    Add functionality to enter operator
    -Set a flag to reset when number is typed in after operator
    -Create an array to store display numbers and operators
    -Add main function to logically combine all functions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let calculationArray = [];
+let operatorPressed = false;
+
 function enterNumber() {
   const maxLength = 17; // User can enter only 17 before display overflows
 
@@ -6,8 +9,13 @@ function enterNumber() {
   const numbers = document.querySelectorAll(".number");
   numbers.forEach((number) =>
     number.addEventListener("click", (e) => {
-      if (display.textContent.length < maxLength)
+      if (display.textContent.length < maxLength) {
+        if (operatorPressed) {
+          display.textContent = "";
+          operatorPressed = false;
+        }
         display.textContent += e.target.textContent;
+      }
     })
   );
 }
@@ -20,3 +28,29 @@ function enterDecimal() {
     if (display.textContent.indexOf(".") == -1) display.textContent += ".";
   });
 }
+
+function enterOperator() {
+  const display = document.querySelector(".display");
+
+  const operators = document.querySelectorAll(".operator");
+  operators.forEach((operator) =>
+    operator.addEventListener("click", (e) => {
+      if (display.textContent) {
+        operatorPressed = true;
+
+        // Push the current number into array
+        calculationArray.push(display.textContent);
+        // Push the operator into the array
+        calculationArray.push(e.target.textContent);
+      }
+    })
+  );
+}
+
+function main() {
+  enterNumber();
+  enterDecimal();
+  enterOperator();
+}
+
+main();
