8739474d1b2c6192449289661b1e9e13d4eb1486
Chetan <165123379+mn-chetan@users.noreply.github.com>
2024-10-17 20:55:15 +0530

    Add function to perform simple operations
    Display the running calculation

--- javascript.js
+++ javascript.js
@@ -43,8 +43,45 @@ function enterOperator() {
         // Push the operator into the array
         calculationArray.push(e.target.textContent);
       }
+
+      if (calculationArray.length == 4) {
+        const result = operate(
+          calculationArray[1],
+          Number(calculationArray[0]),
+          Number(calculationArray[2])
+        );
+        display.textContent = result;
+        calculationArray.splice(0, 3, result);
+      }
     })
   );
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function percent(a) {
+  return a / 100;
+}
+
+function operate(operator, a, b) {
+  if (operator == "+") return add(a, b);
+  else if (operator == "-") return subtract(a, b);
+  else if (operator == "*") return multiply(a, b);
+  else if (operator == "/") return divide(a, b);
 }
 
 function main() {
